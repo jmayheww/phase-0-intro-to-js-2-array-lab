e49a6c284f0a24cccc7f88e6b95962da8e2b7b35
Joshua <joshsmayhew@gmail.com>
2022-06-13 13:32:43 +0700

    commit tree f857b996dc6d04685b7b1f3fce0b2a6e65f0bcfb

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function arrayFunctions() {
+  beforeEach(function () {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+  });
+}
+
+function destructivelyAppendCat(newCat) {
+  return cats.push(newCat);
+}
+
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(newCat) {
+  return cats.unshift(newCat);
+}
+destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat(lostCat) {
+  return cats.pop(lostCat);
+}
+destructivelyRemoveLastCat("Garfield");
+
+function destructivelyRemoveFirstCat(lostCat) {
+  return cats.shift(lostCat);
+}
+
+destructivelyRemoveFirstCat("Otis");
+
+function appendCat(name) {
+     const copyOfCats = [...cats, name];
+     return copyOfCats;
+}
+
+appendCat("Broom");
+
+function prependCat(name) {
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+prependCat('Arnold');
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+removeLastCat();
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+removeFirstCat();
